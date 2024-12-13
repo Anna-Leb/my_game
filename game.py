c6bf3e109e117f2e9ca4f07bb8b16c11d149495b
Anna-Leb <banankaanna@gmail.com>
2024-12-13 22:25:03 +0500

    Добавлена проверка выполнения условия победы

--- game.py
+++ game.py
@@ -26,13 +26,27 @@ while running:
       x, y = pygame.mouse.get_pos()
             grid_x = x // cell_size
             grid_y = y // cell_size
-
+  
+  screen.fill((255, 255, 255))
+  
   for i in range (4):
     for j in range (4):
       color = field[i][j]
       pygame.draw.circle(screen, color, (i * cell_size + cell_size // 2, y * cell_size + cell_size //2), cell_size // 2 - 5)
-  screen.fill((255, 255, 255))
-  
+
+  all_same = True
+  for row in field:
+    for cell in row:
+      if cell != field[0][0]:
+        all_same = False
+    if not all_same:
+      break
+
+  if all_same:
+    font = pygame.font.Font(None, 50)
+    text = font.render("Задача решена!", True, (0, 0, 0))
+    screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
+
   pygame.display.flip()    # для обновления экрана
   
 pygame.quit()
